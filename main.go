b0616023f15bfab073a0a54a5e6db0a05d18145c
son <takomarine417@gmail.com>
2020-02-18 22:12:22 +0900

    append other

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package chitchat
 
 import (
+	"html/template"
 	"net/http"
 )
 
@@ -12,11 +13,40 @@ func main() {
 	files := http.FileServer(http.Dir("/public"))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
+	// index
 	mux.HandleFunc("/", index)
+	// error
+	mux.HandleFunc("/err", err)
+
+	// defined in route_auth.go
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	// defined in route_thread.go
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
 
 	server := &http.Server {
 		Addr:              "0.0.0.0:8080",
 		Handler:           mux,
 	}
 	server.ListenAndServe()
+}
+
+// HTMLを生成してResponseWriterに書き出してる
+func index(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"templates/layout.html",
+		"templates/navbar.html",
+		"templates/index.html",
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	threads, err := data.Threads(); if err == nil {
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
 }
